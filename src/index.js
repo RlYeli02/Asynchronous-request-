6f0918f61dea0fb2c02eab90c903f88d37f45129
franyeliza <frodriguez@icq247.com>
2021-04-22 16:23:02 -0400

    fixing

--- src/index.js
+++ src/index.js
@@ -3,27 +3,28 @@ var req = new XMLHttpRequest();
 var url = 'http://localhost:10000/image/image-3.jpg';
 
 function GetImages (){
-       //ASINCRONA
-      var req = new XMLHttpRequest();
-      var url = 'http://localhost:10000/image/image-3.jpg';
 
-      req.open('GET', url, true);
+  //ASINCRONA
+  var req = new XMLHttpRequest();
+  var url = 'http://localhost:10000/image/image-3.jpg';
 
-      req.onreadystatechange = function () {
+  req.open('GET', url, true);
+  req.onreadystatechange = function () {
      
-      if (req.readyState==4){
-        if (req.status===200) 
-        {
-          var imagen = document.createElement("img")
-          imagen.src = req.response
-          imagen.className ="CuteDoggo";
-          getImagen.appendChild(imagen)
-        }
-        else{
-          console.log("not found")
-        }
-
+    if (req.readyState==4){
+      if (req.status===200) 
+      {
+        console.log(req)
+        var imagen = document.createElement("img")
+        imagen.src = req.response
+        imagen.className ="CuteDoggo";
+        getImagen.appendChild(imagen)
       }
-    };
-    req.send(null); 
+      else {
+        console.log("not found")
+      }
+
+    }
+  };
+  req.send(null); 
 }
