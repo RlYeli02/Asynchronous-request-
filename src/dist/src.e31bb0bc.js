81c3fbb2608934e8ab071958f531d65218ca779b
franyeliza <frodriguez@icq247.com>
2021-04-22 18:12:33 -0400

    sincrona

--- src/dist/src.e31bb0bc.js
+++ src/dist/src.e31bb0bc.js
@@ -120,23 +120,76 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"index.js":[function(require,module,exports) {
 var getImagen = document.getElementById("imagen-table");
 
-function GetImages() {
+function GetImagesAsync() {
   //ASINCRONA
   var req = new XMLHttpRequest();
-  req.open('GET', 'http://localhost:10000/image/image-1.jpg'); // req.open('GET', 'http://localhost:10000/image/image-2.jpg');
-  // req.open('GET', 'http://localhost:10000/image/image-3.jpg');
-  // req.open('GET', 'http://localhost:10000/image/image-4.jpg');
-  // req.open('GET', 'http://localhost:10000/image/image-5.jpg');
-  // req.open('GET', 'http://localhost:10000/image/image-6.jpg');
-  // req.open('GET', 'http://localhost:10000/image/image-7.jpg');
-  // req.open('GET', 'http://localhost:10000/image/image-8.jpg');
-  // req.open('GET', 'http://localhost:10000/image/image-9.jpg');
+  var req2 = new XMLHttpRequest();
+  var req3 = new XMLHttpRequest();
+  var req4 = new XMLHttpRequest();
+  req.open('GET', 'http://localhost:10000/image/image-1.jpg');
+  req2.open('GET', 'http://localhost:10000/image/image-2.jpg');
+  req3.open('GET', 'http://localhost:10000/image/image-3.jpg');
+  req4.open('GET', 'http://localhost:10000/image/image-4.jpg');
 
   req.onreadystatechange = function () {
+    console.log(req, "req");
+
     if (req.readyState == 4) {
       if (req.status === 200) {
         var res = req.response;
-        console.log(res);
+        var imagen = document.createElement("img");
+        imagen.src = res;
+        imagen.className = "CuteDoggo";
+        getImagen.appendChild(imagen);
+      } else {
+        console.log("not found");
+      }
+    }
+  }; //Req 2
+
+
+  req2.onreadystatechange = function () {
+    console.log(req2, "req2");
+
+    if (req2.readyState == 4) {
+      if (req2.status === 200) {
+        var res = req2.response;
+        var imagen = document.createElement("img");
+        imagen.src = res;
+        imagen.className = "CuteDoggo";
+        getImagen.appendChild(imagen);
+      } else {
+        console.log("not found");
+      }
+    }
+  }; // Req 3
+
+
+  req3.onreadystatechange = function () {
+    console.log(req3, "req3");
+
+    if (req3.readyState == 4) {
+      if (req3.status === 200) {
+        var res = req3.response; // console.log(res)
+
+        var imagen = document.createElement("img");
+        imagen.src = res;
+        imagen.className = "CuteDoggo";
+        getImagen.appendChild(imagen);
+      } else {
+        console.log("not found");
+      }
+    }
+  }; // Req 4
+
+
+  req4.onreadystatechange = function () {
+    console.log(req4, "req4");
+
+    if (req4.readyState == 4) {
+      if (req4.status === 200) {
+        var res = req4.response; // console.log(res)
+
         var imagen = document.createElement("img");
         imagen.src = res;
         imagen.className = "CuteDoggo";
@@ -148,10 +201,77 @@ function GetImages() {
   };
 
   req.send(null);
+  req2.send(null);
+  req3.send(null);
+  req4.send(null);
+}
+
+function GetImagesSync() {
+  //ASINCRONA
+  var req = new XMLHttpRequest();
+  var req2 = new XMLHttpRequest();
+  var req3 = new XMLHttpRequest();
+  var req4 = new XMLHttpRequest();
+  req.open('GET', 'http://localhost:10000/image/image-1.jpg');
+  req2.open('GET', 'http://localhost:10000/image/image-2.jpg');
+  req3.open('GET', 'http://localhost:10000/image/image-3.jpg');
+  req4.open('GET', 'http://localhost:10000/image/image-4.jpg');
+
+  if (req.status === 200) {
+    var res = req.response;
+    var imagen = document.createElement("img");
+    imagen.src = res;
+    imagen.className = "CuteDoggo";
+    getImagen.appendChild(imagen);
+  } else {
+    console.log("not found");
+  } //Req 2
+
+
+  if (req2.status === 200) {
+    var res = req2.response;
+    var imagen = document.createElement("img");
+    imagen.src = res;
+    imagen.className = "CuteDoggo";
+    getImagen.appendChild(imagen);
+  } else {
+    console.log("not found");
+  } // Req 3
+
+
+  if (req3.status === 200) {
+    var res = req3.response; // console.log(res)
+
+    var imagen = document.createElement("img");
+    imagen.src = res;
+    imagen.className = "CuteDoggo";
+    getImagen.appendChild(imagen);
+  } else {
+    console.log("not found");
+  } // Req 4
+
+
+  if (req4.status === 200) {
+    var res = req4.response; // console.log(res)
+
+    var imagen = document.createElement("img");
+    imagen.src = res;
+    imagen.className = "CuteDoggo";
+    getImagen.appendChild(imagen);
+  } else {
+    console.log("not found");
+  }
+
+  req.send(null);
+  req2.send(null);
+  req3.send(null);
+  req4.send(null);
 }
 
 var h = document.getElementById("klk");
-h.onclick = GetImages;
+h.onclick = GetImagesAsync;
+var a = document.getElementById("klkSync");
+a.onclick = GetImagesSync;
 },{}],"../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -180,7 +300,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58199" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51982" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
