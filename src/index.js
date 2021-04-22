ee3718084db001a8b1414edf92cc39cd6041c8a2
franyeliza <frodriguez@icq247.com>
2021-04-22 18:08:11 -0400

    making buttons sync and async

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var getImagen = document.getElementById("imagen-table")
 
-function GetImages (){
+function GetImagesAsync (){
     //ASINCRONA
     var req  = new XMLHttpRequest();
     var req2 = new XMLHttpRequest();
@@ -10,17 +10,13 @@ function GetImages (){
     req2.open('GET', 'http://localhost:10000/image/image-2.jpg');
     req3.open('GET', 'http://localhost:10000/image/image-3.jpg');
     req4.open('GET', 'http://localhost:10000/image/image-4.jpg');
-    // req.open('GET', 'http://localhost:10000/image/image-5.jpg');
-    // req.open('GET', 'http://localhost:10000/image/image-6.jpg');
-    // req.open('GET', 'http://localhost:10000/image/image-7.jpg');
-    // req.open('GET', 'http://localhost:10000/image/image-8.jpg');
-    // req.open('GET', 'http://localhost:10000/image/image-9.jpg');
+ 
     req.onreadystatechange = function () {
+      console.log(req, "req")
       if (req.readyState==4){
         if (req.status===200) 
         {
           var res = req.response
-          // console.log(res)
           var imagen = document.createElement("img")
           imagen.src = res
           imagen.className ="CuteDoggo";
@@ -34,11 +30,12 @@ function GetImages (){
 
     //Req 2
     req2.onreadystatechange = function () {
+      console.log(req2, "req2")
       if (req2.readyState==4){
         if (req2.status===200) 
         {
           var res = req2.response
-          // console.log(res)
+         
           var imagen = document.createElement("img")
           imagen.src = res
           imagen.className ="CuteDoggo";
@@ -52,6 +49,7 @@ function GetImages (){
 
     // Req 3
     req3.onreadystatechange = function () {
+      console.log(req3, "req3")
       if (req3.readyState==4){
         if (req3.status===200) 
         {
@@ -70,6 +68,7 @@ function GetImages (){
 
     // Req 4
     req4.onreadystatechange = function () {
+      console.log(req4, "req4")
       if (req4.readyState==4){
         if (req4.status===200) 
         {
@@ -90,5 +89,90 @@ function GetImages (){
     req3.send(null); 
     req4.send(null); 
 }
+
+function GetImagesSync (){
+  //ASINCRONA
+  var req  = new XMLHttpRequest();
+  var req2 = new XMLHttpRequest();
+  var req3 = new XMLHttpRequest();
+  var req4 = new XMLHttpRequest();
+  req.open('GET', 'http://localhost:10000/image/image-1.jpg');
+  req2.open('GET', 'http://localhost:10000/image/image-2.jpg');
+  req3.open('GET', 'http://localhost:10000/image/image-3.jpg');
+  req4.open('GET', 'http://localhost:10000/image/image-4.jpg');
+
+  
+      if (req.status===200) 
+      {
+        var res = req.response
+        var imagen = document.createElement("img")
+        imagen.src = res
+        imagen.className ="CuteDoggo";
+        getImagen.appendChild(imagen)
+      }
+      else{
+        console.log("not found")
+      }
+    
+  
+
+  //Req 2
+  
+      if (req2.status===200) 
+      {
+        var res = req2.response
+       
+        var imagen = document.createElement("img")
+        imagen.src = res
+        imagen.className ="CuteDoggo";
+        getImagen.appendChild(imagen)
+      }
+      else{
+        console.log("not found")
+      }
+    
+  
+
+  // Req 3
+  
+      if (req3.status===200) 
+      {
+        var res = req3.response
+        // console.log(res)
+        var imagen = document.createElement("img")
+        imagen.src = res
+        imagen.className ="CuteDoggo";
+        getImagen.appendChild(imagen)
+      }
+      else{
+        console.log("not found")
+      }
+    
+  
+
+  // Req 4
+ 
+      if (req4.status===200) 
+      {
+        var res = req4.response
+        // console.log(res)
+        var imagen = document.createElement("img")
+        imagen.src = res
+        imagen.className ="CuteDoggo";
+        getImagen.appendChild(imagen)
+      }
+      else{
+        console.log("not found")
+      }
+    
+  
+  req.send(null); 
+  req2.send(null); 
+  req3.send(null); 
+  req4.send(null); 
+}
 var h=document.getElementById("klk");
-h.onclick = GetImages
+h.onclick = GetImagesAsync
+
+var a=document.getElementById("klkSync");
+a.onclick = GetImagesSync
